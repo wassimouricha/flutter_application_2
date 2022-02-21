da9c5e3491006da978b12b8def9716d5485f2e0c
wassimouricha <wbouricha5@gmail.com>
2022-02-21 16:41:53 +0100

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -9,76 +9,84 @@ class Wassimus extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Application de wassim",
-      theme: Themes().theme1,
-      home: Scaffold(
-        // resizeToAvoidBottomInset: false, //ici cette commande permet de supprimer l'erreur indiquand que le clavier du tel/tablette cache le contenu de l' appli
-        appBar: AppBar(
-          title: Text("Wassim's app"),
-        ),
-        body: SingleChildScrollView(child:  //cette commande ici permet d'indiquer au tel/tab que l'on peut scroller dans l'app
-        Padding(
-          padding: EdgeInsets.all(30),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Login",
-                style: TextStyle(fontSize: 45),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 15),
-                child: TextField(
-                  decoration: InputDecoration(
-                      hintText:
-                          "Tapez votre nom"), //textfield est un champ de texte
-                ),
-              ),
-              TextField(
-                decoration: InputDecoration(hintText: "Mot de passe"),
-                obscureText:
-                    true, //obscureText permet de chacher le texte c'est un booleen donc true or false
-              ),
-              // Row(
-              //   children: [
-              //iciavec row je fais une sorte de container horizontale avec comme enfant les deux widget bouton que j'ai crée précédement
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: ElevatedButton(
-                  onPressed: boutonLoginAppuyer,
-                  child: Text(
-                      "Login"), //child est un widget et donc on lui indique ce que l'on veut à l'intérieur
-                ),
-              ), // elevatedbutton un simple bouton
-              // Spacer(), //le spacer va creer un espace entre les deux boutons
-              Column(children: [
-                 Divider(thickness: 1.5) , //divider est le trait permettant de diviser et de faire un espace entre mes deux bouttons
-                 Padding(padding: EdgeInsets.all(8),
-                 child: Text("Ou"),),
-                  Divider(thickness: 1.5,)
-              ],),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 15),
-                child: TextField(
-                  decoration: InputDecoration(
-                      hintText:
-                          "Nom"), //textfield est un champ de texte
-                ),
-              ),
-             
-              Padding(
-                  padding: EdgeInsets.all(10),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print("le bouton > en tant qu'invité à été appuyé !");
-                    },
-                    child: Text("Continuer en tant qu'invité"),
-                  )),
-            ],
+        title: "Application de wassim",
+        theme: Themes().theme1,
+        home: Scaffold(
+          // resizeToAvoidBottomInset: false, //ici cette commande permet de supprimer l'erreur indiquand que le clavier du tel/tablette cache le contenu de l' appli
+          appBar: AppBar(
+            title: Text("Wassim's app"),
           ),
-        ),
-      ),)
-    );
+          body: SingleChildScrollView(
+            child: //cette commande ici permet d'indiquer au tel/tab que l'on peut scroller dans l'app
+                Padding(
+              padding: EdgeInsets.all(30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Login",
+                    style: TextStyle(fontSize: 45),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText:
+                              "Tapez votre nom"), //textfield est un champ de texte
+                    ),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(hintText: "Mot de passe"),
+                    obscureText:
+                        true, //obscureText permet de chacher le texte c'est un booleen donc true or false
+                  ),
+                  // Row(
+                  //   children: [
+                  //iciavec row je fais une sorte de container horizontale avec comme enfant les deux widget bouton que j'ai crée précédement
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: ElevatedButton(
+                      onPressed: boutonLoginAppuyer,
+                      child: Text(
+                          "Login"), //child est un widget et donc on lui indique ce que l'on veut à l'intérieur
+                    ),
+                  ), // elevatedbutton un simple bouton
+                  // Spacer(), //le spacer va creer un espace entre les deux boutons
+                  Column(
+                    children: [
+                      Divider(
+                          thickness:
+                              1.5), //divider est le trait permettant de diviser et de faire un espace entre mes deux bouttons
+                      Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Text("Ou"),
+                      ),
+                      Divider(
+                        thickness: 1.5,
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Nom"), //textfield est un champ de texte
+                    ),
+                  ),
+
+                  Padding(
+                      padding: EdgeInsets.all(10),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print("le bouton > en tant qu'invité à été appuyé !");
+                        },
+                        child: Text("Continuer en tant qu'invité"),
+                      )),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 
   void boutonLoginAppuyer() {
