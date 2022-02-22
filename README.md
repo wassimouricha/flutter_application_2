ac923d6a97b0b39fa49f3ec94b7e1b32a77f019b
wassimouricha <wbouricha5@gmail.com>
2022-02-22 10:51:19 +0100

    v13

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # My first app
 
-A new Flutter project.
+This is my first and a  new Flutter project.
 
 ## Getting Started
 
@@ -15,4 +15,4 @@ For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
 
-This is my first flutter application  
+
--- lib/customTextFIeld.dart
+++ lib/customTextFIeld.dart
@@ -2,20 +2,27 @@ import 'package:flutter/material.dart';
 
 class CustomTextFIeld extends StatelessWidget {
   CustomTextFIeld(
-      {this.padding = EdgeInsets.zero , this.hinText = "" , this.obscureText = false});//ici on va creer un padding , un hinText et un ObscureText spécifique 
-      
+      {this.padding = EdgeInsets.zero,
+      this.hinText = "",
+      this.obscureText =
+          false}); //ici on va creer un padding , un hinText et un ObscureText spécifique
 
   EdgeInsets padding;
   String hinText;
-  bool obscureText;  //bool est un booleen
+  bool obscureText; //bool est un booleen
+
+  String? errorMessage;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: padding, //le padding spécifique est ici intégré au widget
       child: TextField(
-        decoration: InputDecoration(hintText: hinText),
-        obscureText: obscureText, //obscureText permet de chacher le texte c'est un booleen donc true or false
+        decoration: InputDecoration(
+          hintText: hinText,
+        ),
+        obscureText:
+            obscureText, //obscureText permet de chacher le texte c'est un booleen donc true or false
       ),
     );
   }
