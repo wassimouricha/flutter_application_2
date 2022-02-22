a169eac16803edf6866b0a0e8b5d67a95a4af985
wassimouricha <wbouricha5@gmail.com>
2022-02-22 10:56:43 +0100

    zz

--- lib/customTextFIeld.dart
+++ lib/customTextFIeld.dart
@@ -4,23 +4,26 @@ class CustomTextFIeld extends StatelessWidget {
   CustomTextFIeld(
       {this.padding = EdgeInsets.zero,
       this.hinText = "",
-      this.obscureText =
-          false}); //ici on va creer un padding , un hinText et un ObscureText spécifique
+      this.obscureText = false,  
+          this.longueurMaximale,
+          this.longueurMinimale}); //ici on va creer un padding , un hinText et un ObscureText spécifique
 
   EdgeInsets padding;
   String hinText;
   bool obscureText; //bool est un booleen
 
-  String? errorMessage;
+  int? longueurMinimale;
+  int? longueurMaximale;
+
+  String?
+      errorMessage; //? indique la variable peut etre un null ou ne pas etre un null
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: padding, //le padding spécifique est ici intégré au widget
       child: TextField(
-        decoration: InputDecoration(
-          hintText: hinText,
-        ),
+        decoration: InputDecoration(hintText: hinText, errorText: errorMessage),
         obscureText:
             obscureText, //obscureText permet de chacher le texte c'est un booleen donc true or false
       ),
