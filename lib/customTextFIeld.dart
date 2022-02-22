0531d4d89422fd2e31d96ed9cc522b28a8e8b13c
wassimouricha <wbouricha5@gmail.com>
2022-02-22 10:46:05 +0100

    ajout  de parametre pour hintext obscure text et padding

--- lib/customTextFIeld.dart
+++ lib/customTextFIeld.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFIeld extends StatelessWidget {
-  CustomTextFIeld({this.padding = EdgeInsets.zero}); //ici on va creer un padding spécifique
+  CustomTextFIeld(
+      {this.padding = EdgeInsets.zero , this.hinText = "" , this.obscureText = false});//ici on va creer un padding , un hinText et un ObscureText spécifique 
+      
 
-  EdgeInsets padding; 
+  EdgeInsets padding;
+  String hinText;
+  bool obscureText;  //bool est un booleen
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: padding, //le padding spécifique est ici intégré au widget 
+      padding: padding, //le padding spécifique est ici intégré au widget
       child: TextField(
-        decoration: InputDecoration(hintText: "Mot de passe"),
-        obscureText:
-            true, //obscureText permet de chacher le texte c'est un booleen donc true or false
+        decoration: InputDecoration(hintText: hinText),
+        obscureText: obscureText, //obscureText permet de chacher le texte c'est un booleen donc true or false
       ),
     );
   }
